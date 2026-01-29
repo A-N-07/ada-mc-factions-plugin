ccf1307ac9d94c757d63bcaef866232e58806cfa
A-N-07 <avm.nooren1@ad-academie.nl>
2026-01-29 15:46:44 +0100

    initial commit: set up ada-factions plugin with basic structure and configuration

---
+++ ada-factions/src/main/java/nl/a_n_07/adaFactions/AdaFactions.java
@@ -0,0 +1,17 @@
+package nl.a_n_07.adaFactions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdaFactions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
